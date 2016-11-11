7b3d500d4236ae2782b98c4ed61f444bc741669e
Tim Handy <tjhandy@gmail.com>
2016-11-11 17:56:08 +0000

    Not working

--- server.js
+++ server.js
@@ -1,111 +1,18 @@
-
-
-// fallthrough route
-app.use(function(req, res) {
-    res.sendStatus(404)
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 'use strict'
 
-// init project
-let express = require('express')
-app.get('', function(req, res) {
-    
-})
+let routes = require('./routes')
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-let app = express()
-let port = process.env.PORT || 3000
-//let path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
 
-//app.use(express.static(path.join(__dirname, 'public')))
+console.log('Successfully connected to MongoDB.')
 
-function createNaturalDate(dateObject) {
-    let month = dateObject.getMonth()
-    let date = dateObject.getDate()
-    let year = dateObject.getFullYear()
-    let monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December'
-  ]
-    let naturalDate = monthNames[month] + ' ' + date + ', ' + year
-    return naturalDate
-}
-
-// app.get('/', function(req, res) {
-//     res.send("I'm running")
-// })
-
-app.get('/', function(req, res) {
-    res.sendFile('index.html', {'root': __dirname})
-})
-
-app.get('/:time', function(req, res) {
-    let time = req.params.time
-    let unixTime
-    let naturalDate
-
-    if (time.match(/[0-9]+/)[0].split('').length === time.length) {
-        app.get('/:time', function(req, res) {
-            let time = req.params.time
-            let unixTime
-            let naturalDate
-
-            if (time.match(/[0-9]+/)[0].split('').length === time.length) {
-                unixTime = parseInt(time)
-                let dateObject = new Date(time * 1000)
-                naturalDate = createNaturalDate(dateObject)
-
-            } else if (Date.parse(time)) {
-                let dateObject = new Date(time)
-                unixTime = Date.parse(time) / 1000
-                naturalDate = createNaturalDate(dateObject)
-
-            } else {
-                unixTime = null
-                naturalDate = null
-            }
-
-            let output = {
-                'unix': unixTime,
-                'natural': naturalDate
-            }
-            res.send(output)
-        })
-        unixTime = parseInt(time)
-        let dateObject = new Date(time * 1000)
-        naturalDate = createNaturalDate(dateObject)
-
-    } else if (Date.parse(time)) {
-        let dateObject = new Date(time)
-        unixTime = Date.parse(time) / 1000
-        naturalDate = createNaturalDate(dateObject)
-
-    } else {
-        unixTime = null
-        naturalDate = null
-    }
-
-    let output = {
-        'unix': unixTime,
-        'natural': naturalDate
-    }
-    res.send(output)
-})
+routes(app)
 
 // listen for requests
-let listener = app.listen(port, function() {
+let listener = app.listen(port, process.env.IP, function() {
     console.log('Your app is listening on port ' + listener.address().port)
 })
