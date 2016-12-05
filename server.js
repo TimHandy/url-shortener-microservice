6db2d6584aec62cb9bbbde71c0cfaf14471e0750
Tim Handy <tjhandy@gmail.com>
2016-12-05 21:06:58 +0000

    Change IP

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const ip = process.env.IP || 'localhost'
+// const ip = process.env.IP || 'localhost'
 
 // static files route
 app.use(express.static(path.join(__dirname, 'public')))
@@ -15,6 +15,6 @@ console.log('Successfully connected to MongoDB.')
 routes(app)
 
 // listen for requests
-let listener = app.listen(port, ip, function() {
+let listener = app.listen(port, process.env.IP, function() {
     console.log('Your app is listening at ' + ip + ' ' + listener.address().port)
 })
