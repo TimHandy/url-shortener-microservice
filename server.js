a43b09e674229718b634e0ad31074a74775068ae
Tim Handy <tjhandy@gmail.com>
2016-12-05 21:12:36 +0000

    Finally working on Heroku

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+// Heroku did NOT like the next line, so removed it and put the app.listen line back to process.env.IP. Was getting an 'Error R10 (Boot timeout) -> Web process failed to bind to $PORT within 60 seconds of launch'
 // const ip = process.env.IP || 'localhost'
 
 // static files route
